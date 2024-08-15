f349eb91170835a85dbd8d89e49e479684a85045
jhomoreira <jho@outlook.com>
2024-08-14 21:55:18 -0300

    ajuste função automação express

--- fe/src/environments/environment.prod.ts
+++ fe/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   version: '1.0.0',
   ownUrl: 'https://rapidstream.top',
-  apiUrl: 'https://rapistream.top:5000',
+  apiUrl: 'https://rapistream.top:5000/',
   enableDebug: false
 };
